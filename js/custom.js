e7a6413b477b58134c464a5fbef5b945f4654f32
masud <softcoding.info@gmail.com>
2021-04-25 03:01:43 +0600

    concomitant

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+				$('#menu').slicknav();
+				
+			
+				// slider Start....
+				$('.bxslider').bxSlider({ 
+					auto:true, 
+					mode:'horizontal',
+					captions:true,
+					autoDirection:'prev',
+					maxSlides:3, 
+					
+				});
+});
